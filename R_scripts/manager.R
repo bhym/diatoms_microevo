0fe8b667393f1033a7ca561bc31b97af2e44334e
arfalas <bruno.haymele@szn.it>
2024-04-02 15:32:30 +0200

    build figure folder

--- R_scripts/manager.R
+++ R_scripts/manager.R
@@ -14,7 +14,7 @@ source("Fig1.R")
 #   * substituted 1e-04 with 0.0001
 #   * stage highlight/notation
 ggsave("F1.svg", zoom_Rpl_mu, device = "svg",
-       path = "svgs", scale = 3, width = 8, height = 6,
+       path = "../svgs/raw", scale = 3, width = 8, height = 6,
        units = "cm", dpi = 800)
 
 # Fig2 is plo
@@ -23,21 +23,21 @@ ggsave("F1.svg", zoom_Rpl_mu, device = "svg",
 #   * changed the legend point size (and realign points).
 source("Fig3.R")
 ggsave("F2.svg", plo, device = "svg",
-       path = "svgs", scale = 3, width = 16, height = 12,
+       path = "../svgs/raw", scale = 3, width = 16, height = 12,
        units = "cm", dpi = 800)
 
 #Fig3 is final
 # inkscape edit: align the legend title with the gradient
 source("Fig4.R") #takes time!
 ggsave("F3.svg", final, device = "svg",
-       path = "svgs", scale = 3, width = 8, height = 6,
+       path = "../svgs/raw", scale = 3, width = 8, height = 6,
        units = "cm", dpi = 800)
 
 #Fig4 is Hpl_mu
 # Inkscape edits:
 #   * substituted 1e-04 with 0.0001
 ggsave("F4.svg", Hpl_mu, device = "svg",
-       path = "svgs", scale = 3, width = 8, height = 6,
+       path = "../svgs/raw", scale = 3, width = 8, height = 6,
        units = "cm", dpi = 800)
 
 #Fig S1 is supfig
@@ -45,5 +45,5 @@ ggsave("F4.svg", Hpl_mu, device = "svg",
 #   * Make thicker the line that mark N in the "denser simulation"
 source("supfig.R")
 ggsave("S1.svg", supfig, device = "svg",
-       path = "svgs", scale = 5, width = 8, height = 6,
+       path = "../svgs/raw", scale = 5, width = 8, height = 6,
        units = "cm", dpi = 800)
